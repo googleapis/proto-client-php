310be3490c91f24bde399902077e896e2a358794
michaelbausor <michaelbausor@google.com>
2018-03-14 15:53:07 -0700

    Add dlp v2 (#61)
    * Add dlp v2
    * Update rename and apply to dlp

--- rename.php
+++ rename.php
@@ -32,11 +32,7 @@
 
 /**
  * This script performs find/replace operations on the generated protoc
- * output. The current list of changes is:
- * - Rename class Empty to EmptyC (empty is PHP reserved keyword)
- * - Rename Case to CaseC (case is PHP reserved keyword)
- * - Rename LIST to LIST_ (list is PHP reserved keyword)
- * - Rename <X>Client to <X>GrpcClient (<X>Client conflicts with GAPIC client)
+ * output.
  *
  * NOTE: As new cases are handled, this script must remain idempotent.
  *
@@ -50,14 +46,9 @@ $reg = new RegexIterator($it, '/^.+\.php$/i', RecursiveRegexIterator::GET_MATCH)
 foreach ($reg as $files) {
     $file = $files[0];
     $str = file_get_contents($file);
-    $str = str_replace("\\Google\\Protobuf\\Empty", "\\Google\\Protobuf\\GPBEmpty", $str);
-    $str = str_replace("const LIST", "const GPBLIST", $str);
-    $str = preg_replace("{class (\w+?)(Grpc)?Client}", "class $1GrpcClient", $str);
+    $str = str_replace("const LIST ", "const PBLIST ", $str);
+    $str = str_replace("const AND ", "const PBAND ", $str);
+    $str = str_replace("const NEW ", "const PBNEW ", $str);
+    $str = str_replace("const DEFAULT ", "const PBDEFAULT ", $str);
     file_put_contents($file, $str);
-    if (preg_match("{(\w+?)(Grpc)?Client.php}", $file)) {
-        $file_dest = preg_replace("{(\w+?)(Grpc)?Client.php}", "$1GrpcClient.php", $file);
-        if ($file_dest != $file) {
-            shell_exec("mv $file $file_dest");
-        }
-    }
 }
